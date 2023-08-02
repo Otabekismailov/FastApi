6172c85dcb75318bf2669469d6cf6bf23b1dfa45
Otabekismailov <15ismailov1995@gmail.com>
2023-08-03 04:20:50 +0500

    new

---
+++ pdf to word/data.py
@@ -0,0 +1,21 @@
+""" pip install aspose-words """
+
+"""pdf to docs """
+
+# import aspose.words as aw
+#
+# doc = aw.Document("времена_глаголов.pdf")
+# doc.save("времена_глаголов.docx")
+#
+
+
+"""Jpg to Pdf"""
+
+# import aspose.words as aw
+#
+# doc = aw.Document('cv.png')
+# builder = aw.DocumentBuilder(doc)
+#
+# builder.insert_image('cv.png')
+
+# doc.save("Output.pdf")
